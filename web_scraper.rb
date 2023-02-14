1236edb30fea75d8ba50d2d21341a96c944b1b19
OOsipovs <olegs.osipovs@gmail.com>
2023-02-14 23:01:49 +0200

    Export to json file added

--- web_scraper.rb
+++ web_scraper.rb
@@ -1,6 +1,7 @@
 require 'open-uri'
 require 'nokogiri'
 require 'csv'
+require 'json'
 
 def scrapping
     html = URI.open("https://github.com/search?q=ruby+web+scraping")
@@ -14,22 +15,31 @@ def scrapping
         items_array << [name, description]
     end
 
-    export(items_array)
+    #export_to_csv(items_array)
+    
     #items_array.each_with_index do |item, index|
     #    puts "--------------"
     #    puts "#{index+1} - #{item[0]}"
     #    puts "#{item[1]}"
     #end
 
+    export_to_json(items_array)
 end
 
-def export(items_array)
+def export_to_csv(items_array)
     file_path = "data.csv"
     CSV.open(file_path,'w') do |csv|
         csv << ['#', 'Name', 'Description']
-        items_array.each_with_index do |item|
+        items_array.each_with_index do |item, index|
             csv << [index+1, item[0], item[1]]
         end
+    end
+end
+
+def export_to_json(items_array)
+    file_path = "data.json"
+    File.open(file_path, 'w') do |f|
+        f.write(JSON.pretty_generate(items_array))
     end
 end
 
